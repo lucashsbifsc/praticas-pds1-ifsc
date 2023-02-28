ee486eb879b3f6e794b48fb6a2c7f1da05841088
lucashsantosb <lucas.hsb@aluno.ifsc.edu.br>
2023-02-28 08:57:51 -0300

    Lista1

---
+++ Lista1/src/Conceituais.java
@@ -0,0 +1,13 @@
+/* 
+	1) Um código-fonte que foi produzido por desenvolvedores que possibilita adicionar novas funções ao código-fonte do software.
+
+	2) É uma biblioteca do Java para criar interfaces gráficas.
+ 
+	3)a) Botão = JOptionButton
+	b) Campo de Texto
+	c) Caixa de Seleção
+	d) Barra de Progresso
+	e) Botão de Opção
+	f) Caixa de Combinação
+
+ * */
---
+++ Lista1/src/Questao1.java
@@ -0,0 +1,5 @@
+import javax.swing.JOptionPane;
+
+public class Questao1 {
+	JOptionPane.showInputDialog("Olá");
+}
