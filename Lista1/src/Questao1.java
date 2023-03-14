17d479cd5d333dd033df39eb05377ae449445aea
lucashsantosb <lucas.hsb@aluno.ifsc.edu.br>
2023-03-14 09:03:42 -0300

    Lista1

--- Lista1/src/Questao1.java
+++ Lista1/src/Questao1.java
@@ -1,5 +1,11 @@
 import javax.swing.JOptionPane;
 
 public class Questao1 {
-	JOptionPane.showInputDialog("Olá");
+	public static void main(String[] args) {
+		Double precoLitro = Double.valueOf(JOptionPane.showInputDialog("Insira o preço do litro: "));
+		Integer quantidadeLitros = Integer.valueOf(JOptionPane.showInputDialog("Insira a quantidade de litros: "));
+
+	private static void totalPagemento() {
+		
+	}
 }
